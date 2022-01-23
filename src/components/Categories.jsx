2882557d14dfe5fd71bf0536ff2c0eb27ce670f9
Nikita Shvetsov <nshvtsv@gmail.com>
2022-01-23 03:24:43 -0800

    add some changes

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,51 +1,69 @@
 import React from "react";
 
-class Categories extends React.Component {
-  state = {
-    activeItem: 3,
+function Categories({ items }) {
+  const [activeItem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
   };
-  selectItem = (index) => {
-    this.setState({
-      activeItem: index,
-    });
-  };
-  render() {
-    const { items } = this.props;
-    return (
-      <div>
-        <ul>
-          {items.map((item, index) => {
-            return (
-              <li
-                className={this.state.activeItem === index ? "active" : ""}
-                onClick={() => this.selectItem(index)}
-                key={`${item}_${index}`}
-              >
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+  console.log(activeItem);
+  return (
+    <div>
+      <ul>
+        <li
+          className={activeItem === null ? "active" : ""}
+          onClick={() => {
+            onSelectItem(null);
+          }}
+          className="active"
+        >
+          Все
+        </li>
+        {items.map((item, index) => {
+          return (
+            <li
+              className={activeItem === index ? "active" : ""}
+              onClick={() => onSelectItem(index)}
+              key={`${item}_${index}`}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-// function Categories({ items, onClickItem }) {
-//   return (
-//     <div>
-//       <ul>
-//         <li className="active">Все</li>
-//         {items.map((item, index) => {
-//           return (
-//             <li onClick={() => onClickItem(item)} key={`${item}_${index}`}>
-//               {item}
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </div>
-//   );
+// class Categories extends React.Component {
+//   state = {
+//     activeItem: 0,
+//   };
+//   selectItem = (index) => {
+//     this.setState({
+//       activeItem: index,
+//     });
+//   };
+//   render() {
+//     const { items } = this.props;
+//     return (
+//       <div>
+//         <ul>
+//           {items.map((item, index) => {
+//             return (
+//               <li
+//                 className={this.state.activeItem === index ? "active" : ""}
+//                 onClick={() => this.selectItem(index)}
+//                 key={`${item}_${index}`}
+//               >
+//                 {item}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     );
+//   }
 // }
 
 export default Categories;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Categories from "./Categories";
 import Button from "./Button";
+import SortPopup from "./SortPopup";
 
-export { Header, Categories, Button };
+export { Header, Categories, Button, SortPopup };
