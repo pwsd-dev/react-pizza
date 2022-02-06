a058c815344b585716ae82276e30aa271dd20c88
Nikita Shvetsov <nshvtsv@gmail.com>
2022-02-05 22:43:00 -0800

    cleaned and add styles for phone

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 
-function Search({ onSearch, activeSearch, items }) {
+function Search({ onSearch }) {
   const input = React.useRef();
 
-  // findActiveSearch = items.find((obj) => obj.name === activeSearch).name;
-
-  // console.log(findActiveSearch);
-
   const searchItem = (name) => {
-    // console.log(input.current.value);
-    // console.log(onSearch);
-    // input.current.value = input;
-
     name = input.current.value;
 
     onSearch(name);
-
-    // console.log(input);
   };
 
   return (
-    <div>
+    <div className="search">
       <input ref={input} />
       <button onClick={searchItem}>Поиск</button>
     </div>
